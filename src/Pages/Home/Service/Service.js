2654fa5feb6818affc9b854700c43b3721197ff2
RafiulAlam <mrafiul.alam7@gmail.com>
2021-10-18 13:38:47 +0600

    service error handle

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
-import './Service.css';
 
 const Service = props => {
   console.log(props.service);
   const { name, img, description } = props.service;
   return (
-    <Col xs={12} md={12} lg={12} className="single-card">
-      <div className="d-flex align-items-center justify-content-between">
+    <Col xs={12} md={12} lg={12}>
+      <div>
         <img src={img} alt="" />
-        <div className="mx-2">
+        <div>
           <h2>{name}</h2>
           <p>{description}</p>
         </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
-import useServiceDb from '../../UseServiceDb/useServiceDb';
 import Service from '../Service/Service';
 
 const Services = () => {
-  const [services] = useServiceDb();
-  console.log(services);
+  const [services, setServices] = useState();
+  useEffect(() => {
+    fetch('./fakeDb.json')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setServices(data);
+      });
+  }, []);
+
   return (
     <div className="mt-5">
       <h3>We Specialized In</h3>
       <Container fluid>
         <Row>
           {services.map(service => (
-            <Service service={service}></Service>
+            <Service service={service} key={service.id}></Service>
           ))}
         </Row>
       </Container>
